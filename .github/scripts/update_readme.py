e5e0ec56ab2a988a1e27241c1e40a5e8b3405fb4
apollner <46967911+apollner@users.noreply.github.com>
2023-07-05 21:39:28 +0300

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,7 +1,8 @@
 import requests
 
 # Fetch temperature data
-response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Tel%20Aviv&appid=d59c4359779fe1b6e95b7f9cb73edf6e')
+api_key = os.getenv('WEATHER_API_KEY')
+response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q=Tel%20Aviv&appid={api_key}')
 data = response.json()
 temperature = data['main']['temp'] - 273.15
 

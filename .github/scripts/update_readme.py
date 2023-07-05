5d71b20a8b367041066ec5b8363de373c266fe10
apollner <46967911+apollner@users.noreply.github.com>
2023-07-05 18:26:56 +0300

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,10 +1,10 @@
 import requests
 
 # Fetch temperature data
-response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=d59c4359779fe1b6e95b7f9cb73edf6e')
+response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Tel%20Aviv&appid=d59c4359779fe1b6e95b7f9cb73edf6e')
 data = response.json()
-temperature = data['main']['temp']
+temperature = data['main']['temp'] - 273.15
 
 # Update README
 with open('../../README.md', 'w') as f:
-    f.write(f'Current temperature in London: {temperature}K')
+    f.write(f'Current temperature in Tel Aviv: {temperature}C')

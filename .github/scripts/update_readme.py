ac66101e215b58a714639a86efdd4eee2c666ea2
apollner <46967911+apollner@users.noreply.github.com>
2023-07-05 21:23:33 +0300

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -5,6 +5,9 @@ response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Tel%20
 data = response.json()
 temperature = data['main']['temp'] - 273.15
 
+# Round the temperature to the nearest integer
+temperature = round(temperature)
+
 # Update README
 with open('README.md', 'w') as f:
-    f.write(f'Current temperature in Tel Aviv: {temperature}C')
+    f.write([![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&width=435&lines=Hello+%F0%9F%91%8B+welcome+to+my+GitHub+%F0%9F%94%A5)](https://git.io/typing-svg)+f'Current temperature in Tel Aviv: {temperature}C')

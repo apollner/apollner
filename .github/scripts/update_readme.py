1ad18c3aa72146b856218dc394f806e291c551b6
apollner <46967911+apollner@users.noreply.github.com>
2023-07-06 17:58:25 +0300

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -2,8 +2,8 @@ import requests
 from datetime import datetime
 import os
 import pytz
-
-
+from PIL import Image
+from io import BytesIO
 
 # Fetch temperature data
 api_key = os.getenv('WEATHER_API_KEY')
@@ -20,6 +20,14 @@ now = datetime.now(tel_aviv)
 current_date = now.strftime("%Y-%m-%d")
 current_time = now.strftime("%H:%M")
 
+# Fetch a random image from Lorem Picsum
+response = requests.get('https://picsum.photos/5000')
+image_data = response.content
+image = Image.open(BytesIO(image_data))
+
+# Save the image
+image.save('random_image.jpg')
+
 # Read existing README content
 with open('README.md', 'r') as f:
     content = f.read()
@@ -34,6 +42,7 @@ if index != -1:
 
 # Append the temperature data and the current time to the README content
 content += f'\nCurrent temperature in Tel Aviv: {temperature}°C, recorded on {current_date} at {current_time}\n'
+content += f'\n![Random Image](random_image.jpg)\n'  # Add image to README
 
 # Write the updated content to the README
 with open('README.md', 'w') as f:
